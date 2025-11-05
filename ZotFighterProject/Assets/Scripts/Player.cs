509971df3968d9f4e3b36488663b8ffd56df5142
poloman719 <92735647+poloman719@users.noreply.github.com>
2025-11-04 19:59:50 -0800

    added crouching, tried to add jumping but gave up

--- ZotFighterProject/Assets/Scripts/Player.cs
+++ ZotFighterProject/Assets/Scripts/Player.cs
@@ -6,14 +6,23 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public int speed = 20;
+    // public int jumpFactor = 5;
+    // public float GravConstant = 10;
+    // public float groundDistFactor = 0.01f;
 
-    Vector3 horizontal_velocity;
+    Vector3 velocity;
+    // BoxCollider2D collider;
+
+    // bool onGround = false;
 
     // bool moving = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        // collider = GetComponent<BoxCollider2D>();
+
+        // setup move input action
         var moveAction = new InputAction("move", binding: "<Gamepad>/rightStick");
         
         moveAction.AddCompositeBinding("Dpad")
@@ -23,6 +32,19 @@ public class Player : MonoBehaviour
         moveAction.performed += onMove;
         moveAction.canceled += onCancelMove;
         moveAction.Enable();
+
+        // setup crouch input action
+        var crouchAction = new InputAction("crouch", binding: "<Keyboard>/s");
+
+        crouchAction.performed += onCrouch;
+        crouchAction.canceled += onUnCrouch;
+        crouchAction.Enable();
+
+        // // setup jump input action
+        // var jumpAction = new InputAction("jump", binding: "<Keyboard>/w");
+
+        // jumpAction.performed += onJump;
+        // jumpAction.Enable();
     }
 
     // starts movement of Player when move inputs are performed
@@ -30,10 +52,7 @@ public class Player : MonoBehaviour
     {
         Debug.Log("Player: move started");
         Vector2 input = context.ReadValue<Vector2>();
-        Debug.Log($"Player: y-input: {input.y}");
-        horizontal_velocity = new Vector3(input.x, 0, 0);
-
-        if (input.y < 0) onCrouch();
+        velocity.x = input.x * jumpFactor;
         // moving = true;
     }
 
@@ -41,21 +60,46 @@ public class Player : MonoBehaviour
     void onCancelMove(InputAction.CallbackContext context)
     {
         Debug.Log("Player: move stopped");
-        horizontal_velocity = new Vector3();
+        velocity.x = 0;
         // moving = false;
     }
 
     // implement crouch animation
-    void onCrouch()
+    void onCrouch(InputAction.CallbackContext context)
     {
         // *temporary vertical scaling for crouch
-        // transform.localScale.y *= 0.5;
+        Vector3 scale = transform.localScale;
+        scale.y *= 0.5f;
+        transform.localScale = scale;
     }
+
+    // implement uncrouch animation when input is canceled
+    void onUnCrouch(InputAction.CallbackContext context)
+    {
+        // *temporary vertical scaling for crouch
+        Vector3 scale = transform.localScale;
+        scale.y *= 2.0f;
+        transform.localScale = scale;
+    }
+
+    // adds vertical velocity for jump
+    // void onJump(InputAction.CallbackContext context)
+    // {
+    //     Debug.Log("Player: jumped");
+    //     velocity.y = speed;
+    // }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     Debug.Log($"Player: collided with obj with tag {collision.gameObject.tag}");
+    // }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += horizontal_velocity * Time.deltaTime * speed;
+        // Debug.Log($"velocity: {velocity.x}, {velocity.y}");
+        transform.position += velocity * Time.deltaTime;
+        // if (!onGround) velocity.y -= GravConstant * Time.deltaTime;
         // if (moving) return;
     }
 }
